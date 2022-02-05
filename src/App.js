f07ec24d59970f0a99cf21b037c5fe3542b85051
abimbolataofeek <abimbolaadewale9999@gmail.com>
2022-02-05 08:14:01 +0100

    react hook useState and event commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [showPassword, setShowPassword] = useState(true);
+
+
+    return (
+        <div className='container'>
+            <input
+                type='text'
+                placeholder='Enter Username'
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+            />
+            <input
+                type={showPassword ? 'text' : 'password'}
+                placeholder='Enter Password'
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+
+            <h2>{username}</h2>
+            <h2>{showPassword ? password : '*'.repeat(password.lenght)}</h2>
+            <button onClick={(e) => setShowPassword(!showPassword)}>Show/Hide password</button>
+        </div>
+    )
 }
 
 export default App;
